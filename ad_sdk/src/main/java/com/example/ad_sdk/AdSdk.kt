01e269f217d260fadc2daf0d1dd3372138f1f059
Rick <rick@RickdeMacBook-Pro.local>
2023-09-14 00:13:01 +0800

    [feature]: create timer for impression

--- ad_sdk/src/main/java/com/example/ad_sdk/AdSdk.kt
+++ ad_sdk/src/main/java/com/example/ad_sdk/AdSdk.kt
@@ -3,6 +3,7 @@ package com.example.ad_sdk
 import android.app.Activity
 import android.graphics.Insets
 import android.os.Build
+import android.os.CountDownTimer
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.WindowInsets
@@ -11,6 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 object AdSdk {
     fun init(activity: Activity, recyclerView: RecyclerView, position: Int) {
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            private var isTimerStart = false
+            private var countDownTimer: CountDownTimer? = null
+
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 val adView = recyclerView.layoutManager?.findViewByPosition(position)
@@ -18,11 +23,37 @@ object AdSdk {
                 adView?.getLocationOnScreen(rect)
                 adView?.let {
                     if ((getScreenHeight(activity) - rect[1]) > (adView.measuredHeight / 2)) {
-                        Log.d("@@", "count timer start")
+                        if (isTimerStart.not()) {
+                            Log.d("@@", "count timer start")
+                            isTimerStart = true
+                            countDownTimer = createTimer()
+                            countDownTimer?.start()
+                        } else {
+                            //noting to do
+                        }
                     } else {
                         Log.d("@@", "count timer stop")
+                        isTimerStart = false
+                        countDownTimer?.cancel()
+                        countDownTimer = null
                     }
                 }
+            }
+
+            private fun createTimer(): CountDownTimer {
+                val countDownTimer = object : CountDownTimer(10000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        Log.d("@@", "count timer onTick ${millisUntilFinished / 1000}")
+                    }
+
+                    override fun onFinish() {
+                        Log.d("@@", "impression")
+                        //todo impression callback to client
+                        isTimerStart = false
+                        countDownTimer = null
+                    }
+                }
+                return countDownTimer
             }
         })
     }
